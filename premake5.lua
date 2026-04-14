7c5e975c8cab7094a319c8b55cbc007b3709b24f
BadAcronym <terribleacronym@gmail.com>
2026-04-14 09:31:19 +0200

    add build files :D

---
+++ premake5.lua
@@ -0,0 +1,148 @@
+---@diagnostic disable: undefined-global, undefined-field
+
+workspace("puddle")
+    configurations({"debug", "asan", "release"})
+    platforms({"linux", "windows"})
+    location("build")
+    architecture("x86_64")
+
+project("puddle")
+    language("C")
+    cdialect("C99")
+    warnings("Extra")
+    targetname("puddle")
+    kind("StaticLib")
+
+    filter("configurations:asan")
+        defines{"ASAN"}
+
+    filter("configurations:debug")
+        defines{"DEBUG"}
+
+    filter("configurations:debug or asan")
+        runtime("debug")
+        symbols("On")
+        optimize("Off")
+
+    filter("configurations:release")
+        defines{"NDEBUG"}
+        staticruntime("off")
+        runtime("release")
+        symbols("Off")
+        optimize("Speed")
+
+    filter("platforms:Linux")
+        system("Linux")
+        defines("BUILD_LINUX")
+        targetdir("bin/%{cfg.buildcfg}")
+        objdir("obj/puddle")
+        files({ "./src/linux_*",
+                "./include/linux_*",
+                "./src/string_view*",
+                "./include/string_view*" })
+        includedirs({ "./include/", "/usr/include/"})
+        buildoptions({"-Wextra", "-Wall", "-Wpedantic", "-Wconversion"})
+        linkoptions("-fuse-ld=mold")
+        toolset("clang")
+
+    filter("platforms:Windows")
+        system("Windows")
+        defines("BUILD_WINDOWS")
+        targetdir("bin/%{cfg.buildcfg}")
+        objdir("obj/")
+        files({ "./src/win32_*",
+                "./include/win32_*",
+                "./src/string_view*",
+                "./include/string_view*" })
+        includedirs({ "./include/"})
+
+    filter({"platforms:Linux", "configurations:debug or asan"})
+        buildoptions({"-gfull", "-O1"})
+        linkoptions({"-gfull", "-O1"})
+
+    filter({"platforms:Linux", "configurations:asan"})
+        buildoptions({"-fsanitize=address,leak,undefined", "-fno-omit-frame-pointer",
+                      "-static-libasan"})
+        linkoptions({"-fsanitize=address,leak,undefined", "-fno-omit-frame-pointer",
+                     "-static-libasan"})
+
+    filter({"platforms:Windows", "configurations:asan"})
+        editandcontinue("Off")
+        buildoptions({"/fsanitize=address", "/Zi", "/INCREMENTAL:NO"})
+
+    filter({"platforms:Windows", "configurations:release"})
+        linkoptions({"/NODEFAULTLIB:MSVCRTD"})
+
+project("svtest")
+    language("C")
+    cdialect("C99")
+    warnings("Extra")
+    targetname("svtest")
+    kind("ConsoleApp")
+
+    filter("configurations:asan")
+        defines{"ASAN"}
+
+    filter("configurations:debug")
+        defines{"DEBUG"}
+
+    filter("configurations:debug or asan")
+        staticruntime("off")
+        runtime("debug")
+        symbols("On")
+
+    filter("configurations:release")
+        defines{"NDEBUG"}
+        staticruntime("off")
+        runtime("release")
+        symbols("Off")
+        optimize("Speed")
+
+    filter("platforms:Linux")
+        system("Linux")
+        defines("BUILD_LINUX")
+        targetdir("bin/%{cfg.buildcfg}")
+        objdir("obj/svtest/")
+        files({ "./src/linux_string_view*",
+                "./include/linux_string_view*",
+                "./src/string_view*",
+                "./include/string_view*" })
+        includedirs({ "./include/", "/usr/include/", "./vendor/"})
+        libdirs("./bin/%{cfg.buildcfg}/")
+        buildoptions({"-Wextra", "-Wall", "-Wpedantic", "-Wconversion"})
+        links("puddle:static")
+        linkoptions({"-fuse-ld=mold", "-lm"})
+        toolset("clang")
+
+    filter("platforms:Windows")
+        system("Windows")
+        defines("BUILD_WINDOWS")
+        targetdir("bin/%{cfg.buildcfg}")
+        objdir("obj/")
+        files({ "./src/win32_string_view*",
+                "./include/win32_string_view*",
+                "./src/string_view*",
+                "./include/string_view*" })
+        includedirs({"./include/"})
+        libdirs("./bin/%{cfg.buildcfg}/")
+        links("puddle.lib")
+
+    filter({"platforms:Linux", "configurations:debug or asan"})
+        buildoptions({"-gfull", "-O1"})
+        linkoptions({"-gfull", "-O1"})
+
+    filter({"platforms:Linux", "configurations:asan"})
+        buildoptions({"-fsanitize=address,leak,undefined", "-fno-omit-frame-pointer",
+                      "-static-libasan"})
+        linkoptions({"-fsanitize=address,leak,undefined", "-fno-omit-frame-pointer",
+                     "-static-libasan"})
+
+    filter({"platforms:Windows", "configurations:debug or asan"})
+        kind("ConsoleApp")
+
+    filter({"platforms:Windows", "configurations:asan"})
+        editandcontinue("Off")
+        buildoptions({"/fsanitize=address", "/Zi", "/INCREMENTAL:NO"})
+
+    filter({"platforms:Windows", "configurations:release"})
+        linkoptions({"/NODEFAULTLIB:MSVCRTD"})

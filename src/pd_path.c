ffca6f7193d5817b57692cd8022bfaf9a4b90245
BadAcronym <terribleacronym@gmail.com>
2026-07-17 10:36:08 +0200

    separate into header / source code for less headache

--- src/pd_path.c
+++ src/pd_path.c
@@ -1,43 +1,4 @@
-#ifndef PD_PATH_H
-#define PD_PATH_H
-
-#include <stdint.h>
-
-#ifdef BUILD_LINUX
-    #include <sys/stat.h>
-    #include <dirent.h>
-#endif
-
-#include "string_view.h"
-
-#define PD_TYPE_ERROR     0
-#define PD_TYPE_FILE      1
-#define PD_TYPE_DIRECTORY 2
-#define PD_TYPE_OTHER     3
-
-// will verify if a given path exists. Will return `PD_TYPE_ERROR`, if it does not
-// exist, `PD_TYPE_FILE` or `PD_TYPE_DIRECTORY` if it does, and `PD_TYPE_OTHER` in any
-// other case.
-uint8_t pdVerifyPath
-(
-    StringView path
-);
-
-// will expand the given path into buf, resolving '.', '$HOME' and '~', but currently no
-// other symbols.
-void pdExpandPath
-(
-    const char *path,
-    char       *buf
-);
-
-// lists all files in a directory and writes them into buf, returning a StringView.
-// Separated by ';'. Not recursive.
-StringView pdListFiles
-(
-    StringView directory,
-    char       *buf
-);
+#include "pd_path.h"
 
 uint8_t pdVerifyPath
 (
@@ -285,4 +246,3 @@ StringView pdListFiles
 //         .data = list,
 //     };
 // }
-#endif

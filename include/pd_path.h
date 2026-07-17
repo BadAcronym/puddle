91398fa0b13dda95fd02566e8e47414b130a38d1
BadAcronym <terribleacronym@gmail.com>
2026-07-17 10:07:45 +0200

    copy river2D code now, change def position

--- include/pd_path.h
+++ include/pd_path.h
@@ -7,7 +7,6 @@
     #include <sys/stat.h>
 #endif
 
-#define STRING_VIEW_IMPL
 #include "string_view.h"
 
 #define PD_TYPE_ERROR     0
@@ -30,9 +29,19 @@ void pdExpandPath
     const char *path,
     char       *buf
 );
+
+// lists all files in a directory and writes them into buf, returning a StringView.
+// Separated by ';'. Not recursive.
+StringView pdListFiles
+(
+    StringView directory,
+    char       *buf
+);
 #endif
 
 #ifdef PD_PATH_IMPL
+#define STRING_VIEW_IMPL
+#include "string_view.h"
 uint8_t pdVerifyPath
 (
     StringView path
@@ -59,6 +68,24 @@ uint8_t pdVerifyPath
     return PD_TYPE_OTHER;
 #endif
 #ifdef BUILD_WINDOWS
+    struct _stat pathInfo;
+
+    if(_stat(path, &pathInfo))
+    {
+        return RV_TYPE_ERROR;
+    }
+
+    if(_S_IFDIR & pathInfo.st_mode)
+    {
+        return RV_TYPE_DIRECTORY;
+    }
+
+    if(_S_IFREG & pathInfo.st_mode)
+    {
+        return RV_TYPE_FILE;
+    }
+
+    return RV_TYPE_OTHER;
 #endif
 }
 
@@ -128,4 +155,135 @@ void pdExpandPath
         buf[i] = '\0';
     }
 }
+
+StringView pdListFiles
+(
+    StringView directory
+){
+    DIR           *dir;
+    struct dirent *ent;
+    uint32_t      listSize = 0;
+
+    char path[4096] = {0};
+
+    sv_cstr(directory, path);
+
+    if((dir = opendir(path)))
+    {
+        while((ent = readdir(dir)))
+        {
+            uint8_t length = 0;
+            for(; length < 255 && ent->d_name[length] != '\0'; ++length)
+            {
+            }
+            listSize += length + 1;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "\n\033[31;1;7mERROR: failed to open directory.\033[0m\n");
+        return(StringView){0};
+    }
+
+    String list = {0};
+    list.data   = (char*)malloc(listSize + 1);
+    list.size   = 0;
+    uint32_t offset = 0;
+
+    free(dir);
+    dir = opendir(path);
+
+    while((ent = readdir(dir)))
+    {
+        uint8_t curr_length = 0;
+        for(; curr_length >= 0 && ent->d_name[curr_length] != '\0'; ++curr_length)
+        {
+            list.data[offset + curr_length] = ent->d_name[curr_length];
+        }
+        list.data[offset + curr_length] = ';';
+        list.size += curr_length + 1;
+        offset    += curr_length + 1;
+    }
+
+    list.data[offset] = '\0';
+
+    StringView result =
+    {
+        .data = list.data,
+        .size = list.size
+    };
+
+    char *sorted = calloc(4096, 1);
+    sv_sort_by_delim(result, ';', sorted);
+
+    free((void*)result.data);
+    result.data = sorted;
+
+    free(dir);
+
+    return result;
+}
+
+// windows version of the old API
+// StringView pdListFiles
+// (
+//     StringView directory
+// ){
+//     WIN32_FIND_DATAA fileData;
+//     HANDLE           foundHandle;
+//
+//     char *winPath         = malloc(path.size + 3);
+//     for(uint16_t i = 0; i < path.size; ++i)
+//     {
+//         winPath[i] = path[i];
+//     }
+//     winPath[path.size]     = '\\';
+//     winPath[path.size + 1] = '*';
+//     winPath[path.size + 2] = '\0';
+//
+//     foundHandle = FindFirstFileA(winPath, &fileData);
+//     if(foundHandle == INVALID_HANDLE_VALUE)
+//     {
+//         fprintf(stderr, "\n\033[31;1;7mERROR: failed to open directory.\033[0m\n");
+//         return 0;
+//     }
+//
+//     uint32_t listSize = 0;
+//
+//     while(FindNextFileA(foundHandle, &fileData))
+//     {
+//         uint8_t length = 0;
+//         for(; length < 255 && fileData.cFileName[length] != '\0'; ++length)
+//         {
+//         }
+//         listSize += length + 1;
+//     }
+//
+//     FindClose(foundHandle);
+//     foundHandle = FindFirstFileA(winPath, &fileData);
+//
+//     char     *list  = (char*)malloc(listSize + 1);
+//     uint32_t offset = 0;
+//
+//     while(FindNextFileA(foundHandle, &fileData))
+//     {
+//         uint8_t length = 0;
+//         for(; length < 255 && fileData.cFileName[length] != '\0'; ++length)
+//         {
+//             list[offset + length] = fileData.cFileName[length];
+//         }
+//         list[offset + length] = ';';
+//         offset += length + 1;
+//     }
+//
+//     list[offset] = '\0';
+//
+//     FindClose(foundHandle);
+//     free(winPath);
+//     return(StringView)
+//     {
+//         .data = list,
+//     };
+// }
+
 #endif

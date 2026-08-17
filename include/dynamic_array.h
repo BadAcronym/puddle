9c023d13d93a4dcd6a87c2a86fc1f1f5a3742dff
BadAcronym <terribleacronym@gmail.com>
2026-08-17 11:52:48 +0200

    declare dynamic array files

---
+++ include/dynamic_array.h
@@ -0,0 +1,6 @@
+#ifndef PD_DYNARR_H
+#define PD_DYNARR_H
+
+#define PD_ARR_INIT_SIZE 32
+
+#endif
---
+++ src/dynamic_array.c
@@ -0,0 +1,21 @@
+#include "dynamic_array.h"
+
+void pdArrPush
+(
+){
+}
+
+void pdArrRemove
+(
+){
+}
+
+void pdArrReserve
+(
+){
+}
+
+void pdArrClear
+(
+){
+}

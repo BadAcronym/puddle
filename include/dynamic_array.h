4821232169783986184e291e9ac83f108a2932ed
BadAcronym <terribleacronym@gmail.com>
2026-08-27 07:45:10 +0200

    sketch out array functions

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -1,6 +1,41 @@
 #ifndef PD_DYNARR_H
 #define PD_DYNARR_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 #define PD_ARR_INIT_SIZE 32
+
+typedef struct DynArr
+{
+    void     *mem;
+    size_t   size;
+    size_t   reserved;
+    uint16_t typeSize;
+}
+DynArr;
+
+extern void pdPushArr
+(
+    DynArr *dyn,
+    void   *element
+);
+
+extern void pdRemoveArr
+(
+    DynArr *dyn,
+    void   *element
+);
+
+extern void pdReserveArr
+(
+    DynArr   *dyn,
+    uint64_t count
+);
+
+extern void pdClearArr
+(
+    DynArr *dyn
+);
 
 #endif
--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -1,21 +1,54 @@
+#include <stdio.h>
+
 #include "dynamic_array.h"
 
-void pdArrPush
+void pdPushArr
 (
+    DynArr *dyn,
+    void   *element
 ){
+    if(!dyn)
+    {
+        fprintf(stderr, "\033[31;1mERROR: cannot push to nullptr Dynamic Array."
+                "\033[0m\n");
+        return;
+    }
+    else if(!dyn->typeSize)
+    {
+        fprintf(stderr, "\033[31;1mERROR: typeSize cannot be NULL.\033[0m\n");
+        return;
+    }
+
+    if(!dyn->reserved)
+    {
+        // TODO: alloc
+    }
+    else if(dyn->size + 1 > dyn->reserved)
+    {
+        // TODO: realloc to dyn->reserved * 2
+    }
 }
 
-void pdArrRemove
+void pdRemoveArr
 (
+    DynArr *dyn,
+    void   *element
 ){
+    // TODO: remove specified element from array, which means moving the tail end over
+    // it
 }
 
-void pdArrReserve
+void pdReserveArr
 (
+    DynArr   *dyn,
+    uint64_t count
 ){
+    // TODO: alloc like push
 }
 
-void pdArrClear
+void pdClearArr
 (
+    DynArr *dyn
 ){
+    // TODO: free? set to 0? we'll see
 }
